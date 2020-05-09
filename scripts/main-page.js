4093013ee3216a9db575eef02659fee6cd468f60
FinallyDone <x-boter@mail.ru>
2020-05-09 20:13:59 +0300

    Added 2 sorts, upgraded SetUpElements
    Добавил 2 сортировки:
       -пузырьковая
       -быстрая
    Теперь все элементы отображаются правильно на экране

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -30,13 +30,6 @@ class star {
 
 /* Timer для анимации */
 var Timer;
-// Какая частота обновлений кадров для рендера поля
-var howMuchMilSecondForRendering = 50;
-/*
-    Проверка на изменения в страницы
-    для меньшей загрузки на страницу
-*/
-var ChangesOnPage = false;
 
 /* Массив звезд (элементов массива) */
 var stars = [];
@@ -80,32 +73,28 @@ var imgBackgroundSortingPositionY = 0;
 
 /* Рендер поля с массивом для отображения сортировки */
 function renderSortingField() {
-    if (ChangesOnPage) {
-        let positionX;
-        let positionY;
-        let canvas = document.querySelector(".display-sort");
-        // Рисуем фон
-        ctx.drawImage(imgBackgroundSorting, imgBackgroundSortingPositionX, imgBackgroundSortingPositionY, imgBackgroundSortingWidth, imgBackgroundSortingHeight);
+    let positionX;
+    let positionY;
+    let canvas = document.querySelector(".display-sort");
+    // Рисуем фон
+    ctx.drawImage(imgBackgroundSorting, imgBackgroundSortingPositionX, imgBackgroundSortingPositionY, imgBackgroundSortingWidth, imgBackgroundSortingHeight);
 
-        // Находим угол 
-        let alp = 2 / stars.length * 3.14;
-        // Определяем размер звезд
-        let starWidth, starHeight;
-        if (stars.length >= 16) {
-            starWidth = imgStarWidthMin;
-            starHeight = imgStarHeightMin;
-        } else {
-            starWidth = imgStarWidthMax;
-            starHeight = imgStarHeightMax;
-        }
-        // Рисуем все звезды для наглядной сортировки по окружности
-        for (let i = 0; i < stars.length; i++) {
-            positionX = canvas.width / 2 + 60 * Math.cos(i * alp) * (stars[i].id / stars.length * 4) - 25;
-            positionY = canvas.height / 2 + 60 * Math.sin(i * alp) * (stars[i].id / stars.length * 4) - 25;
-            console.log(positionY)
-            ctx.drawImage(imgStar, positionX, positionY, starWidth, starHeight);
-        }
-        ChangesOnPage = false;
+    // Находим угол 
+    let alp = 2 / stars.length * 3.14;
+    // Определяем размер звезд
+    let starWidth, starHeight;
+    if (stars.length >= 16) {
+        starWidth = imgStarWidthMin;
+        starHeight = imgStarHeightMin;
+    } else {
+        starWidth = imgStarWidthMax;
+        starHeight = imgStarHeightMax;
+    }
+    // Рисуем все звезды для наглядной сортировки по окружности
+    for (let i = 0; i < stars.length; i++) {
+        positionX = canvas.width / 2 + 60 * Math.cos(i * alp) * (stars[i].id / stars.length * 4) - 25;
+        positionY = canvas.height / 2 + 60 * Math.sin(i * alp) * (stars[i].id / stars.length * 4) - 25;
+        ctx.drawImage(imgStar, positionX, positionY, starWidth, starHeight);
     }
 }
 
@@ -115,15 +104,30 @@ function setUpAllElemets() {
     let x, y;
     // Рандом длины массива
     //let howLongArray = GetRandomInt(minArraySize, maxArraySize);
-    let howLongArray = GetRandomInt(6, 40);
+    let howLongArray = GetRandomInt(minArraySize, maxArraySize);
     // Рандомное значение для элемента массива
     let randValue = 0;
+    // Массив, чтобы в будущем определить позиции каждого элемента
+    let tempStars = [];
     // Создаем массив рандомной длины с рандомными элементами
     for (let i = 0; i < howLongArray; i++) {
         // задаем рандомное значение
         randValue = GetRandomInt(minValueSize, maxValueSize);
         stars[i] = new star(randValue);
-        stars[i].setId(i+1);
+        tempStars[i] = randValue;
+    }
+    // Сортируем
+    sortQuick(tempStars);
+    
+    // Определяем позицию для каждого элемента
+    for (let i = 0; i < howLongArray; i++) {
+        for(let j=0;j<tempStars.length;j++){
+            if(tempStars[j] == stars[i].value){
+                stars[i].setId(j);
+                tempStars[j] = 0;
+                break;
+            }
+        }
     }
 }
 
@@ -136,6 +140,54 @@ function setUpAllImgs() {
     imgBackgroundSorting.src = srcImgs + "background-display-sort.png";
 }
 
+/* Быстрая сортировка */
+function sortBubble(array) {
+  for (let j = array.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if (array[i] > array[i + 1]) {
+        Swap(array, i, i + 1);
+      }
+    }
+  }
+}
+
+/* Сортировка - быстрая */
+function sortQuick(array, left, right) {
+    var index;
+    if (array.length > 1) {
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? array.length - 1 : right;
+        index = partition(array, left, right);
+        if (left < index - 1) {
+            sortQuick(array, left, index - 1);
+        }
+        if (index < right) {
+            sortQuick(array, index, right);
+        }
+    }
+}
+
+/* Вспомогательная функция для быстрой сортировки */
+function partition(array, left, right) {
+    let pivot   = array[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+    while (i <= j) {
+        while (array[i] < pivot) {
+            i++;
+        }
+        while (array[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            Swap(array, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
 //////////////////////////////////// Events ///////////////////////////////////////////
 
 /* Загрузка страницы */
@@ -146,8 +198,8 @@ document.addEventListener('DOMContentLoaded', function () {
     setUpAllElemets();
     // Подготовка всех картинок
     setUpAllImgs();
-    // Анимация сортировки массива
-    Timer = setInterval(renderSortingField, howMuchMilSecondForRendering);
+    // Анимация сортировки массива, таймер используется, чтобы дать время подгрузиться картинкам
+    timer = setTimeout(renderSortingField, 70);
 })
 
 //////////////////////////////////// Patterns /////////////////////////////////////////
@@ -157,4 +209,11 @@ function GetRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+// Меняет местами два элемента массива
+function Swap(items, firstIndex, secondIndex){
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
 }
